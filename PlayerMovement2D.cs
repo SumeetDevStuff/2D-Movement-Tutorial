2085468d58aede671f1acc34e3190de517bc2f07
SumeetDevStuff <71875193+SumeetDevStuff@users.noreply.github.com>
2020-09-30 13:56:47 +0530

    Add files via upload

---
+++ PlayerMovement2D.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement2D : MonoBehaviour
+{
+    public float speed;
+    public float jump;
+
+    private float move;
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent <Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move = Input.GetAxis("Horizontal");
+
+        rb.velocity = new Vector2(move * speed, rb.velocity.y);
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(new Vector2(rb.velocity.x, jump));
+        }
+    }
+}
